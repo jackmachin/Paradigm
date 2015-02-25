8482931921e546d82e3a215ce70c7034022f1d2a
jackmachin <jack.machin125@gmail.com>
2015-02-25 17:16:02 +0000

    Actually it's about jpegs in game journalism

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 				<div id="inner-header" class="wrapper">
 
             <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-            <h1 id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" id="logo-image" alt="<?php bloginfo ('name');?> - <?php bloginfo('description'); ?>" title="<?php bloginfo('name'); ?>"></a></h1>
+            <h1 id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.jpg" id="logo-image" alt="<?php bloginfo ('name');?> - <?php bloginfo('description'); ?>" title="<?php bloginfo('name'); ?>"></a></h1>
 
             <?php // if you'd like to use the site description you can un-comment it below ?>
             <?php // bloginfo('description'); ?>
