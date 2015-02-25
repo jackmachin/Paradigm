3f0317f15867b04a0ea6f613e1568f821fe5f081
jackmachin <jack.machin125@gmail.com>
2015-02-25 15:48:18 +0000

    Stripping it all back

--- library/bones.php
+++ library/bones.php
@@ -146,9 +146,9 @@ function bones_scripts_and_styles() {
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/min/scripts.min.js', array( 'jquery' ), '', true );
 
 		// enqueue styles and scripts
-		wp_enqueue_script( 'bones-modernizr' );
-		wp_enqueue_style( 'bones-stylesheet' );
-		wp_enqueue_style( 'bones-ie-only' );
+		//wp_enqueue_script( 'bones-modernizr' );
+		//wp_enqueue_style( 'bones-stylesheet' );
+		//wp_enqueue_style( 'bones-ie-only' );
 
 		$wp_styles->add_data( 'bones-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
 
