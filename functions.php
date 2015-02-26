54dbcf75769d0ab9ffd720b6b0fffeacf7948f4a
jackmachin <jack.machin125@gmail.com>
2015-02-26 13:37:35 +0000

    H2 Widget Titles

--- functions.php
+++ functions.php
@@ -120,8 +120,8 @@ function bones_register_sidebars() {
 		'description' => __( 'The first (primary) sidebar.', 'bonestheme' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget' => '</div>',
-		'before_title' => '<h4 class="widgettitle">',
-		'after_title' => '</h4>',
+		'before_title' => '<h2 class="widgettitle">',
+		'after_title' => '</h2>',
 	));
 
 	/*
