c84f054bf750ceca18df6d4318c88c4f55d5f5dd
jackmachin <jack.machin125@gmail.com>
2015-02-27 11:04:14 +0000

    Broke it

--- partials/post-meta.php
+++ partials/post-meta.php
@@ -1,7 +1,8 @@
 <p class="byline vcard">
   <?php printf( __( 'Posted', 'bonestheme' ) . 
     ' <time itemprop="datePublished" content="2014-07-10" class="updated" datetime="%1$s" pubdate>%2$s</time> ' 
+    . __('by', 'bonestheme' ) . 
     get_the_time('Y-m-j'), 
-    get_the_time(get_option('date_format')),
+    get_the_time(get_option('date_format'))
   ?>
 </p>
