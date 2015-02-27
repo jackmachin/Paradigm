174cf31b0bfd500197f80ee028f97d2a6f144f49
jackmachin <jack.machin125@gmail.com>
2015-02-27 11:21:00 +0000

    Trying to keep it slim

--- single.php
+++ single.php
@@ -5,25 +5,38 @@
 				<div id="inner-content" class="container">
 
 					<div class="main" role="main">
+                      
+              <?php 
+
+                if ( function_exists('yoast_breadcrumb') ) {
+                  yoast_breadcrumb('<div id="breadcrumbs">','</div>');
+                } 
+
+              ?>
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<?php
-								/*
-								 * Ah, post formats. Nature's greatest mystery (aside from the sloth).
-								 *
-								 * So this function will bring in the needed template file depending on what the post
-								 * format is. The different post formats are located in the post-formats folder.
-								 *
-								 *
-								 * REMEMBER TO ALWAYS HAVE A DEFAULT ONE NAMED "format.php" FOR POSTS THAT AREN'T
-								 * A SPECIFIC POST FORMAT.
-								 *
-								 * If you want to remove post formats, just delete the post-formats folder and
-								 * replace the function below with the contents of the "format.php" file.
-								*/
-								get_template_part( 'post-formats/format', get_post_format() );
-							?>
+              <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                
+                <?php get_template_part ('partials/article-header');?>
+
+                <section class="entry-content" itemprop="articleBody">
+                  <?php
+                    the_content();
+                  ?>
+                </section> <?php // end article section ?>
+
+                <footer class="article-footer">
+
+                  <?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
+
+                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+
+                </footer> <?php // end article footer ?>
+
+                <?php comments_template(); ?>
+
+              </article> <?php // end article ?>
 
 						<?php endwhile; ?>
 
