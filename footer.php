093f1991ec050b180bbabffc28c5dd6007e6c799
jackmachin <jack.machin125@gmail.com>
2015-02-27 11:52:06 +0000

    Legal footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
 					<?php get_template_part ('partials/navigation' , 'footer');?>
           
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> Paradigm Partners LLP | Registered in England and Wales. Company No.: OC323405 | Paradigm Partners LLP, Paradigm House, Brooke Court, Wilmslow, Cheshire, SK9 3ND | Paradigm Partners LLP is a Limited Liability Partnership |.</p>
 
 				</div>
 
