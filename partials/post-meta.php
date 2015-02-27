237cc592ffae3791b176dc8842d5452877974080
jackmachin <jack.machin125@gmail.com>
2015-02-27 11:16:52 +0000

    Removing some janky semantic HTML

--- partials/post-meta.php
+++ partials/post-meta.php
@@ -1,6 +1,6 @@
 <p class="byline entry-meta vcard">
   <?php printf( __( 'Posted %1$s', 'bonestheme' ),
   /* the time the post was published */
-  '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
+  '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '">' . get_the_time(get_option('date_format')) . '</time>'
   ); ?>
 </p>
