62b5873b9c1859d50ecc70672574dbb6720941d7
jackmachin <jack.machin125@gmail.com>
2015-02-27 11:06:37 +0000

    Rip it up and start again

--- partials/post-meta.php
+++ partials/post-meta.php
@@ -1,7 +1,8 @@
-<p class="byline vcard">
-  <?php printf( __( 'Posted', 'bonestheme' ) . 
-    ' <time itemprop="datePublished" content="2014-07-10" class="updated" datetime="%1$s" pubdate>%2$s</time> ' 
-    get_the_time('Y-m-j'), 
-    get_the_time(get_option('date_format'))
-  ?>
+<p class="byline entry-meta vcard">
+  <?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
+  /* the time the post was published */
+  '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+  /* the author of the post */
+  '<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+  ); ?>
 </p>
