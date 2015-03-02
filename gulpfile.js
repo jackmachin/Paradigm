5e6a3585172a902c32544c3df341ca59c0a74b9f
jackmachin <jack.machin125@gmail.com>
2015-03-02 14:31:32 +0000

    Trying a new Editor

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
@@ -7,17 +7,19 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
-var imagemin = require ('gulp-imagemin');
+var imagemin = require('gulp-imagemin');
 
 // Lint Task
-gulp.task('lint', function() {
+gulp.task('lint', function () {
+    "use strict";
     return gulp.src('library/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
-gulp.task('sass', function() {
+gulp.task('sass', function () {
+    "use strict";
     return gulp.src('library/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('library/css'))
@@ -27,26 +29,28 @@ gulp.task('sass', function() {
 });
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
+    "use strict";
     return gulp.src('library/js/*.js')
-      .pipe(uglify())
-      .pipe(concat('scripts.js'))
-      .pipe(rename('scripts.min.js'))
-      .pipe(gulp.dest('library/js/min'));
+        .pipe(uglify())
+        .pipe(concat('scripts.js'))
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('library/js/min'));
 });
 
-gulp.task('images', function() {
-  return gulp.src('library/images/*')
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('library/images'))
+gulp.task('images', function () {
+    "use strict";
+    return gulp.src('library/images/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('library/images'));
 });
 
 gulp.task('watch', function () {
-
-  gulp.watch ('./library/js/*.js' , ['lint', 'scripts']);
-  gulp.watch ('./library/js/**/*.js' , ['lint', 'scripts']);
-  gulp.watch('./library/scss/*.scss', ['sass']);
-  gulp.watch('./library/scss/**/*.scss', ['sass']);
+    "use strict";
+    gulp.watch('./library/js/*.js', ['lint', 'scripts']);
+    gulp.watch('./library/js/**/*.js', ['lint', 'scripts']);
+    gulp.watch('./library/scss/*.scss', ['sass']);
+    gulp.watch('./library/scss/**/*.scss', ['sass']);
 
 });
 
