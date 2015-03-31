f9519c0a5fa4d58076f13bb3bf0cb9ceb32cfd22
jackmachin <jack.machin125@gmail.com>
2015-03-31 10:42:57 +0100

    Issues

--- functions.php
+++ functions.php
@@ -198,12 +198,12 @@ external fonts. If you're using Google Fonts, you
 can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
-function bones_fonts() {
-  //wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
-  //wp_enqueue_style( 'googleFonts');
+function paradigm_fonts() {
+  wp_register_style('google-fonts', 'http://fonts.googleapis.com/css?family=Raleway:700,700italic|Open-Sans:400,400italic,700,700italic');
+  wp_enqueue_style( 'google-fonts');
 }
 
-add_action('wp_print_styles', 'bones_fonts');
+add_action('wp_print_styles', 'paradigm_fonts');
 
 
 
